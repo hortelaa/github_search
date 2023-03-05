0d24f95b1537501f3861d5a27c32c1252e0d6635
Lucas Oliveira Vieira Cardoso <lucascardoso5263@gmail.com>
2023-03-05 14:37:05 -0300

    setup: ambiente preparado

--- src/main.js
+++ src/main.js
@@ -1,5 +1,26 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createRouter, createWebHistory } from 'vue-router'
+import { createPinia } from 'pinia'
+
+//Router
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+
+    ],
+})
+
+//Views
+
+
+//Arquivos SCSS
+import '@/assets/styles/main.scss'
+import '@/assets/styles/reset.scss'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+createApp(App)
+    .use(router)
+    .use(createPinia())
+    .mount('#app')

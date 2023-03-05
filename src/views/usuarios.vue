49bbd2c27a95521a957c950974e2e5e06bc59918
Lucas Oliveira Vieira Cardoso <lucascardoso5263@gmail.com>
2023-03-05 15:39:15 -0300

    feat : limitado a lista de usuários para 3 por vez, adicionado botão para ir renderizando mais

--- src/views/usuarios.vue
+++ src/views/usuarios.vue
@@ -1,6 +1,7 @@
 <template>
-    <UserCard v-for="user in usuarios" :key="user.id" :usuarios="user"></UserCard>
+    <UserCard v-for="user in usuariosVisiveis" :key="user.id" :usuarios="user"></UserCard>
     <p v-if="!usuarios.length">Nenhum repositório encontrado.</p>
+    <button v-if="!todosVisiveis" @click="verMais">MOSTRAR MAIS</button>
 </template>
 
 
@@ -9,12 +10,28 @@ import UserCard from "@/components/UserCard.vue";
 import { useMainStore } from "@/store/mainStore"
 
 export default {
+    data() {
+        return {
+            qntdVisivel: 3
+        }
+    },
     components: {
         UserCard
     },
     computed: {
         usuarios() {
             return useMainStore().usuarios;
+        },
+        usuariosVisiveis() {
+            return this.usuarios.slice(0, this.qntdVisivel)
+        },
+        todosVisiveis() {
+            return this.usuariosVisiveis.length === this.usuarios.length
+        }
+    },
+    methods: {
+        verMais() {
+            this.qntdVisivel += 3
         }
     }
 }
